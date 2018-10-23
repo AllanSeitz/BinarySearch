3a2d387675a73dcd7df47ad435b8b02c167d6147
Allan <allan@MacBook-Pro.localdomain>
2018-10-23 09:25:24 -0400

    BinarySearch

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinarySearch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //number range
+      var min = 1;
+      var max = 1000;
+      var guess = "";
+      var pick = (max + min) / 2;
+
+      Console.WriteLine($"Choose a number beetween {min} and {max} remember your number");
+
+      while (guess != "y")
+      {
+        //let user select a number
+        Console.WriteLine($"Is your number higher or lower than or equal to {pick}? choose h/l/y");
+
+        guess = Console.ReadLine();
+        if (guess == "h")
+        {
+          pick = ((pick + max) / 2) + 1;
+        }
+        else if (guess == "l")
+        {
+          pick = ((pick + min) / 2) - 1;
+        }
+        else if (guess == "y")
+        {
+          Console.WriteLine($"Your number is {pick}");
+        }
+        Console.WriteLine($"Is your number higher or lower than or equal to {pick}? choose h/l/y");
+      };
+    }
+  }
+}
